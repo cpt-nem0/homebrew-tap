ff33f69d136fdc98dbf46153ade5caed19090bfd
Rohan <rohanya.76426@gmail.com>
2026-07-25 12:42:44 +0530

    Add verified param to url stanza for audit compliance

--- Casks/verse.rb
+++ Casks/verse.rb
@@ -2,7 +2,8 @@ cask "verse" do
   version "2.0.3"
   sha256 "c155a6eb35861f3846049f16975a810929796ddfec7e4e11140e2ee370d6f1eb"
 
-  url "https://github.com/cpt-nem0/verse/releases/download/v#{version}/Verse.zip"
+  url "https://github.com/cpt-nem0/verse/releases/download/v#{version}/Verse.zip",
+      verified: "github.com/cpt-nem0/verse/"
   name "Verse"
   desc "Time-synced lyrics in a floating, draggable glass pill"
   homepage "https://cpt-nem0.github.io/verse/"
